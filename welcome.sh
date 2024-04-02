f1c0b8363821810f299f6e7dfd8dc059957e7f00
Damon-Lee Pointon <51855558+DLBPointon@users.noreply.github.com>
2024-04-02 14:29:21 +0000

    A Welcome message

---
+++ welcome.sh
@@ -0,0 +1,26 @@
+cat << EOF 
+--------------------------------------------------------
+Welcome to the Manual-Curation Tutorial by Sanger's GRIT
+--------------------------------------------------------
+              First, activate your env!!       
+                "conda activate rapid"             
+--------------------------------------------------------
+            Now you can actually use things!      
+--------------------------------------------------------
+     
+ The Pretext-to-tpf/agp-utils have been aliased to:
+                      "ptt"
+ 
+ Run PretextView it self with:
+                 "./PretextView &"
+
+ The & will make life easier, and try to run it in the
+ VScode window if possible, this will stop any issues 
+ with time out.
+--------------------------------------------------------
+
+            Enjoy the session!!!
+
+--------------------------------------------------------
+
+EOF
