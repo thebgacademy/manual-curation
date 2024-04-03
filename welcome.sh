7c616561f723105d1ff56fab07b496ee4933e552
DLBPointon <damonlbp@hotmail.co.uk>
2024-04-03 14:10:24 +0100

    updates

---
+++ welcome.sh
@@ -0,0 +1,25 @@
+cat << EOF
+--------------------------------------------------------------------------------------------
+                  Welcome to the Manual-Curation Tutorial by Sanger's GRIT
+--------------------------------------------------------------------------------------------
+                                  First, activate your env!!
+                                    "conda activate rapid"
+--------------------------------------------------------------------------------------------
+                              Now you can actually use things!
+--------------------------------------------------------------------------------------------
+ RUN THIS LINE IN THE CLI:
+ alias ptt='python3 /agp-tpf-utils/src/tola/assembly/scripts/pretext_to_tpf.py'
+
+ The Pretext-to-tpf have been aliased to:   "ptt"
+
+ Run PretextView it self with:              "./PretextView &"
+
+ The & will make life easier, and try to run it in the VScode window if possible,
+ this will stop any issues with time out.
+--------------------------------------------------------------------------------------------
+
+                                  Enjoy the session!!!
+
+--------------------------------------------------------------------------------------------
+
+EOF
