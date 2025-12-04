48d76177e834cf8725446c3a45ab0352a2680a2c
Damon-Lee Pointon <51855558+DLBPointon@users.noreply.github.com>
2025-12-04 14:02:28 +0000

    Updated session for 2025

--- welcome.sh
+++ welcome.sh
@@ -2,15 +2,25 @@ cat << EOF
 --------------------------------------------------------------------------------------------
                   Welcome to the Manual-Curation Tutorial by Sanger's GRIT
 --------------------------------------------------------------------------------------------
-                                  First, activate your env!!       
-                                    "conda activate rapid"             
+                      First, activate your env (if not already active)!       
+                                  "conda activate rapid"             
 --------------------------------------------------------------------------------------------
                               Now you can actually use things!      
 --------------------------------------------------------------------------------------------
- RUN THIS LINE IN THE CLI:
- alias ptt='python3 /agp-tpf-utils/src/tola/assembly/scripts/pretext_to_tpf.py'
-  
- The Pretext-to-tpf have been aliased to:   "ptt"
+
+ This Codespace contains everything you need for the Manual-Curation session, including:
+  - Test data in ./test_data & ./rapid-curation-main-test_data
+  - PretextView
+  - agp-tpf-utils scripts accessed by the commands:
+    - asm-format
+      Parses and reformats AGP and TPF files, converting into either format.
+
+    - find-overlaps
+      Finds overlapping entries within AGP or TPF assembly files. Useful for debugging.
+
+    - pretext-to-asm
+      Takes the AGP file output by PretextView and the input assembly (usually FASTA), 
+      and produces an output assembly in FASTA and AGP formats.
  
  Run PretextView it self with:              "./PretextView &"
 
