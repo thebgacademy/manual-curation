20b1066b1edfbeced90ee7816d78e06e7eefc377
DLBPointon <damonlbp@hotmail.co.uk>
2026-04-21 11:56:27 +0100

    Update all

---
+++ intro.sh
@@ -0,0 +1,25 @@
+cat << EOF
+--------------------------------------------------------------------------------------------
+                  Welcome to the Manual-Curation Tutorial by Sanger's GRIT
+--------------------------------------------------------------------------------------------
+                    First, make sure you are in the rapid environment!!!
+--------------------------------------------------------------------------------------------
+                              Now you can actually use things!
+--------------------------------------------------------------------------------------------
+
+ The `pretext-to-asm` has been installed.
+
+ Run PretextView it self with: "./PretextView &"
+
+ The ampersand (`&`) will make life easier, so you don't end up locking up a
+ terminal window just for PretextView!
+
+--------------------------------------------------------------------------------------------
+
+                                  Enjoy the session!!!
+            This environment has been built for The Biodiversity Genomics Academy
+                   an open and free to all bioinformatics education group!
+
+--------------------------------------------------------------------------------------------
+
+EOF
